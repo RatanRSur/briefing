70d588312771717eb557699a9a802ddf6eaad9a2
Ratan Rai Sur <ratan.r.sur@gmail.com>
2018-12-12 18:43:12 -0500

    regex parse all upgrades

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,39 @@ use std::io::{self, BufReader};
 use std::io::prelude::*;
 use std::fs::File;
 
-fn is_upgrade(s: &str) -> bool {
-    s.contains("upgraded")
+use regex::Regex;
+use chrono::naive::NaiveDateTime;
+
+#[derive(Debug)]
+struct Upgrade {
+    timestamp: NaiveDateTime,
+    package_name: String,
+    old_version: String,
+    new_version: String
+}
+
+fn extract_data(s: &str, regex: &Regex) -> Option<Upgrade> {
+    let maybe_line_captures = regex.captures(s);
+    maybe_line_captures.map(|caps| {
+
+        Upgrade { timestamp:    NaiveDateTime::parse_from_str(&caps[1], "%Y-%m-%d %H:%M").unwrap(),
+                package_name: caps[3].to_string(),
+                old_version:  caps[4].to_string(),
+                new_version:  caps[5].to_string()
+        }
+    })
 }
 
 fn main() -> io::Result<()> {
     let f = BufReader::new(File::open("/var/log/pacman.log")?);
 
+    let regex = Regex::new(
+        r"^\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2})\] \[ALPM\] upgraded ([^ ]*) \((.+) -> ((.+))\)$"
+    ).unwrap();
+
     f.lines()
-        .filter_map(|result_str| result_str.ok().filter(|s| is_upgrade(&s)))
-        .for_each(|s| println!("{}", s));
+        .filter_map(|result_str| result_str.map(|s| extract_data(&s, &regex)).unwrap())
+        .for_each(|e| println!("{:?}", e));
 
     Ok(())
 }
