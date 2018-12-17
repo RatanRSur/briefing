1d455a0260799641ff084b02dfa74fead003d0f0
Ratan Rai Sur <ratan.r.sur@gmail.com>
2018-12-17 16:59:38 -0500

    print package and (homepage OR template) URL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
@@ -7,90 +8,101 @@ use std::process::Command;
 use chrono::naive::NaiveDateTime;
 use regex::Regex;
 
-mod url_formats;
+mod url_templates;
 
 #[derive(Debug)]
 pub struct Upgrade {
     timestamp: NaiveDateTime,
-    package_name: String,
+    package: Package,
     old_version: String,
     new_version: String,
+    url: String,
 }
 
-fn extract_data(s: &str, regex: &Regex) -> Option<Upgrade> {
+fn extract_upgrade(s: &str, regex: &Regex, packages: &HashMap<String, Package>) -> Option<Upgrade> {
     let maybe_line_captures = regex.captures(s);
-    maybe_line_captures.map(|caps| Upgrade {
-        timestamp: NaiveDateTime::parse_from_str(&caps[1], "%Y-%m-%d %H:%M").unwrap(),
-        package_name: caps[2].to_string(),
-        old_version: caps[3].to_string(),
-        new_version: caps[4].to_string(),
+    maybe_line_captures.and_then(|caps| {
+        packages.get(&caps[2].to_string()).map(|package| Upgrade {
+            timestamp: NaiveDateTime::parse_from_str(&caps[1], "%Y-%m-%d %H:%M").unwrap(),
+            package: package.clone(),
+            old_version: caps[3].to_string(),
+            new_version: caps[4].to_string(),
+            url: url_templates::format_url(&package.url_template, &caps[4].to_string()),
+        })
     })
+}
+
+#[derive(Debug, PartialEq, Hash, Eq, Clone)]
+struct Package {
+    name: String,
+    url_template: String,
 }
 
 fn main() -> io::Result<()> {
     let last_briefing =
         NaiveDateTime::parse_from_str("2018-12-01 00:00", "%Y-%m-%d %H:%M").unwrap();
 
-    let installed_packages_output = String::from_utf8(
-        Command::new("/usr/bin/pacman")
-            .arg("--query")
-            .arg("--quiet")
-            .arg("--explicit")
-            .output()
-            .expect("failed to execute process")
-            .stdout,
-    )
-    .unwrap();
+    let release_notes_templates_map: HashMap<&str, &str> = url_templates::RELEASE_NOTES_TEMPLATES
+        .iter()
+        .cloned()
+        .collect();
 
-    let installed_packages: HashSet<&str> = installed_packages_output.split_whitespace().collect();
-
-    let upgraded_packages = {
-        let f = BufReader::new(File::open("/var/log/pacman.log")?);
-
-        let regex = Regex::new(
-            r"^\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2})\] \[ALPM\] upgraded ([^ ]*) \((.+) -> ([^-]+)(-\d+)?\)$",
-        )
-        .unwrap();
-
-        f.lines()
-            .filter_map(|result_str| result_str.ok().and_then(|s| extract_data(&s, &regex)))
-            .skip_while(|upgrade| upgrade.timestamp < last_briefing)
-            .filter(|upgrade| installed_packages.contains(&upgrade.package_name.as_ref()))
-            .collect::<Vec<Upgrade>>()
-    };
-
-    let upgraded_package_urls: Vec<String> = {
-        let command_output = String::from_utf8(
+    let installed_packages: HashMap<String, Package> = {
+        let installed_packages_output = String::from_utf8(
             Command::new("/usr/bin/pacman")
                 .arg("--query")
+                .arg("--explicit")
                 .arg("--info")
-                .args(
-                    &upgraded_packages
-                        .iter()
-                        .map(|upgrade| &upgrade.package_name)
-                        .collect::<Vec<&String>>(),
-                )
                 .output()
                 .expect("failed to execute process")
                 .stdout,
         )
         .unwrap();
 
-        let regex = Regex::new(r"\nURL *: (.*)\n").unwrap();
+        let regex = Regex::new(r"(^|\n)(Name|URL) +: (.*)\n").unwrap();
 
-        regex
-            .captures_iter(&command_output)
-            .map(|captures| String::from(&captures[1]))
-            .collect()
+        let mut packages = HashMap::new();
+        let mut captures_iter = regex.captures_iter(&installed_packages_output);
+        while let Some(captures) = captures_iter.next() {
+            let package_name = String::from(&captures[3]);
+            packages.insert(
+                package_name.clone(),
+                Package {
+                    name: package_name,
+                    url_template: release_notes_templates_map
+                        .get(&captures[3])
+                        .unwrap_or(&&captures_iter.next().unwrap()[3])
+                        .to_string(),
+                },
+            );
+        }
+        packages
     };
 
-    //println!("{}", upgraded_package_urls);
-    for (upgrade, _url) in upgraded_packages.iter().zip(upgraded_package_urls.iter()) {
-        println!(
-            "{}: {}",
-            upgrade.package_name,
-            url_formats::get_release_notes_url(&upgrade)
-        );
+    let upgrades = {
+        let f = BufReader::new(File::open("/var/log/pacman.log")?);
+
+        let regex = Regex::new(r"^\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2})\] \[ALPM\] upgraded ([^ ]*) \((.+) -> ([^-]+)(-\d+)?\)$",) .unwrap();
+
+        let installed_package_names: HashSet<_> = installed_packages
+            .iter()
+            .map(|name_and_package| name_and_package.0)
+            .cloned()
+            .collect();
+
+        f.lines()
+            .filter_map(|result_str| {
+                result_str
+                    .ok()
+                    .and_then(|s| extract_upgrade(&s, &regex, &installed_packages))
+            })
+            .skip_while(|upgrade| upgrade.timestamp < last_briefing)
+            .filter(|upgrade| installed_package_names.contains(&upgrade.package.name))
+            .collect::<Vec<Upgrade>>()
+    };
+
+    for upgrade in upgrades {
+        println!("{}: {}", upgrade.package.name, upgrade.url);
     }
 
     Ok(())
---
+++ src/url_templates.rs
@@ -0,0 +1,18 @@
+use std::collections::HashMap;
+use strfmt::strfmt;
+
+use crate::Upgrade;
+
+pub static RELEASE_NOTES_TEMPLATES: &'static [(&str, &str)] = &[(
+    "git",
+    "https://raw.githubusercontent.com/git/git/master/Documentation/RelNotes/{version}.txt",
+)];
+
+pub fn format_url(template: &String, version: &String) -> String {
+    let format_args: HashMap<String, &String> = [(String::from("version"), version)]
+        .iter()
+        .cloned()
+        .collect();
+
+    strfmt(template, &format_args).unwrap_or(String::from(""))
+}
