0b23b6d0ce4fd1f8348f292d9b99e429c8f08453
Ratan Rai Sur <ratan.r.sur@gmail.com>
2018-12-30 23:14:31 -0500

    add ansi_term and bolding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use ansi_term::Color::Purple;
+use ansi_term::Style;
 use lazy_static::lazy_static;
 use std::collections::BTreeMap;
 use std::collections::HashMap;
@@ -133,12 +135,12 @@ fn section_header(margin_width: usize, header: &str) -> String {
     buf.push(' ');
     buf.push_str(header);
     buf.push('\n');
-    buf
+    bold(buf)
 }
 
 fn package_output(margin_width: usize, package_name: &str, urls: &Vec<String>) -> String {
     let mut buf = String::new();
-    buf.push_str(&left_pad_to_width(margin_width, package_name));
+    buf.push_str(&bold(left_pad_to_width(margin_width, package_name)));
     for (i, url) in urls.iter().enumerate() {
         if i != 0 {
             buf.push_str(&left_pad_to_width(margin_width, ""));
@@ -156,6 +158,10 @@ fn left_pad_to_width(width: usize, str: &str) -> String {
     (0..(width - str.len())).for_each(|_| buf.push(' '));
     buf.push_str(str);
     buf
+}
+
+fn bold(str: String) -> String {
+    Style::new().bold().paint(str).to_string()
 }
 
 fn get_upgrades_by_name(
