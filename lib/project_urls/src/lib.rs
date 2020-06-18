edd71c68d082765b44ec0f8288ff0309c1724448
Ratan Rai Sur <ratan.r.sur@gmail.com>
2020-06-18 09:42:23 -0400

    project urls
    Signed-off-by: Ratan Rai Sur <ratan.r.sur@gmail.com>

--- lib/project_urls/src/lib.rs
+++ lib/project_urls/src/lib.rs
@@ -24,17 +24,20 @@ lazy_static! {
         github_releases!("jwilm", "alacritty", "v"),
         ("chromium", "https://chromium.googlesource.com/chromium/src/+log/{old_version}..{version}?pretty=fuller&n=10000"),
         ("feh", "https://feh.finalrewind.org/archive/{version}/"),
-        ("firefox", "https://www.mozilla.org/en-US/firefox/{version}/releasenotes/",),
+        ("firefox", "https://www.mozilla.org/en-US/firefox/{version}/releasenotes/"),
         github_releases!("flatpak", "flatpak", ""),
         ("ghc", "https://downloads.haskell.org/~ghc/{version}/docs/html/users_guide/{version}-notes.html"),
-        ("git", "https://raw.githubusercontent.com/git/git/master/Documentation/RelNotes/{version}.txt",),
+        ("git", "https://raw.githubusercontent.com/git/git/master/Documentation/RelNotes/{version}.txt"),
+        ("github-cli", "https://github.com/cli/cli/releases/tag/v{version}"),
         ("gimp", "https://www.gimp.org/release-notes/gimp-{version}.html"),
         ("go", "https://golang.org/doc/devel/release.html#go{version}"),
         github_releases!("jarun", "googler", ""),
         ("i3-gaps", "https://github.com/Airblader/i3/blob/gaps-next/RELEASE-NOTES-{version}"),
-        ("linux", "https://cdn.kernel.org/pub/linux/kernel/v5.x/ChangeLog-{version}",),
+        ("linux", "https://cdn.kernel.org/pub/linux/kernel/v5.x/ChangeLog-{version}"),
         github_releases!("neovim", "neovim", "v"),
         ("racket", "https://download.racket-lang.org/v{version}.html"),
+        github_releases!("BurntSushi","ripgrep", ""),
+        github_releases!("r-darwish","topgrade", "v"),
         ("tmux", "https://raw.githubusercontent.com/tmux/tmux/{version}/CHANGES"),
         github_releases!("vim", "vim", "v"),
         github_releases!("Jguer", "yay", "v"),
@@ -50,7 +53,9 @@ lazy_static! {
          ("intellij-idea-community-edition", "https://www.jetbrains.com/idea/whatsnew/"),
          ("networkmanager", "https://gitlab.freedesktop.org/NetworkManager/NetworkManager/blob/master/NEWS"),
          ("virtualbox", "https://www.virtualbox.org/wiki/Changelog"),
-         ("vlc", "https://www.videolan.org/developers/vlc-branch/NEWS")]
+         ("vlc", "https://www.videolan.org/developers/vlc-branch/NEWS"),
+         ("yourkit", "https://www.yourkit.com/changes/"),
+         ("zoom", "https://support.zoom.us/hc/en-us/sections/201214205-Release-Notes")]
             .iter()
             .cloned()
             .collect();
